2da9f227f89283d2e0c2790ff1b78bbdedbacdc7
G-shino <shinogu2929@gmail.com>
2020-05-05 16:08:21 +0900

    add color

---
+++ src/Components/GlobalStyle/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styles from "./style";
+
+const { WrapperDiv } = styles;
+
+export const GlobalStyle: React.FC = ({}) => {
+  return (
+    <WrapperDiv>
+      <p>Hello</p>
+    </WrapperDiv>
+  );
+};
---
+++ src/Constants/Color.ts
@@ -0,0 +1,7 @@
+export enum Color {
+  //襲の色目
+  BASE_COLOR = "#FFFFFF",
+  MAIN_COLOR = "#0F3350",
+  ACCENT_COLOR_1 = "#F5F5DC",
+  ACCENT_COLOR_2 = "#E3D7A3",
+}
