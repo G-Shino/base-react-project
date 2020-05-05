935c0a025f3bb27253dc59978fc7804145aa555d
G-shino <shinogu2929@gmail.com>
2020-05-05 23:46:36 +0900

    fix packagejson router gitignore

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Routes } from "./Routes";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./Components/GlobalStyle";
 
 //ライブラリ等でくるむ場所　reduxを導入するならproviderが入る場所
@@ -9,9 +9,9 @@ import { GlobalStyle } from "./Components/GlobalStyle";
 ReactDOM.render(
   <div>
     <GlobalStyle />
-    <HashRouter>
+    <BrowserRouter>
       <Routes />
-    </HashRouter>
+    </BrowserRouter>
   </div>,
   document.getElementById("app")
 );
