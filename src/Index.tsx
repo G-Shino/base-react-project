606499a6fcb5062b4a98029b0b5e35872a62b26f
G-shino <shinogu2929@gmail.com>
2020-05-06 16:37:26 +0900

    browserrouter -> hashrouter

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Routes } from "./Routes";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { GlobalStyle } from "./Components/GlobalStyle";
 
 //ライブラリ等でくるむ場所　reduxを導入するならproviderが入る場所
@@ -9,9 +9,9 @@ import { GlobalStyle } from "./Components/GlobalStyle";
 ReactDOM.render(
   <div>
     <GlobalStyle />
-    <BrowserRouter>
+    <HashRouter>
       <Routes />
-    </BrowserRouter>
+    </HashRouter>
   </div>,
   document.getElementById("app")
 );
